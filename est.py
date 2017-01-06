b72e386b6ae36b37520c642ed81a6d98f7382c05
Maksym Repa <maksymrepa@gmail.com>
2017-01-06 17:42:50 +0200

    Murka

--- est.py
+++ est.py
@@ -29,7 +29,7 @@ while not done:
 
 
             self.hi_tot = tk.Button(self)
-            self.hi_tot["text"] = "Ololo\n(kyrka)"
+            self.hi_tot["text"] = "Ololo\n(murka)"
             self.hi_tot.pack(side="right")
 
             self.hi_rr = tk.Button(self)
